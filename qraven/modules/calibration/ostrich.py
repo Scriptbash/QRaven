5c0a60025fe53a67fd971aac40bb97d0a791378c
Scriptbash <98601298+Scriptbash@users.noreply.github.com>
2023-06-01 16:58:49 -0400

    Load calibration parameters automatically

--- qraven/modules/calibration/ostrich.py
+++ qraven/modules/calibration/ostrich.py
@@ -1,15 +1,17 @@
 from qgis.gui import QgsFileWidget
 from qgis.PyQt.QtWidgets import *
-from ..PyRavenR import readRavenParams, readRVPtemplate, getDefaultParamValue
+from ..PyRavenR import readRavenParams, readRVPtemplate
 from pathlib import Path
 from os import path
 
 
 class Ostrich:
     def __init__(self):
-        pass
+        self.param_matches = []
 
     def add_file_pair(self, dlg):
+        # Adds a new row in the file pairs table
+
         table = dlg.table_filepairs  # Get the file pairs table
         current_row = table.rowCount()  # Get the number of rows the table has
         table.insertRow(current_row)  # Inserts a new row below the last row
@@ -24,34 +26,43 @@ class Ostrich:
         table.resizeColumnsToContents()
 
     def remove_file_pair(self, dlg):
+        # Removes the selected row from the file pairs table
+
         table = dlg.table_filepairs
         selected_row = table.currentRow()
         table.removeRow(selected_row)
 
     def load_parameters(self, dlg):
+        # Loads the parameters in the parameters selection table
         script_dir = Path(__file__).parent.parent.parent
         raven_parameters_file = path.join(script_dir, "ext_data/RavenParameters.dat")
-        rvn_rvp_params = readRavenParams(raven_parameters_file)  # Gathers all rvp params
-        # print(rvn_rvp_params)
-        params_name_list = [i[0] for i in rvn_rvp_params]   # List with all rvp params name
+        rvn_rvp_params = readRavenParams(raven_parameters_file)  # Gathers all possible rvp params
 
-        file_pairs = self.get_file_pairs(dlg)
+        file_pairs = self.get_file_pairs(dlg)   # Extracts get the file pairs (currently support only one pair)
         table = dlg.table_ost_params
 
         self.clear_table(table)
 
+        # TODO finish this loop so multiple file pairs can be used without crashing
         for pairs in file_pairs:
             template = pairs[0]
-            rvp_template_params = readRVPtemplate(template)
+            rvp_template_params = readRVPtemplate(template)  # Read the template file
 
+        rvp_template_clean = []   # Cleans up the list so all parameters needed can match
         for line in rvp_template_params:
-            # for col in line:
-            for param in params_name_list:
-                if param in line:
+            for item in line:
+                rvp_template_clean.append(item.strip(','))
+
+        # Loop that matches the parameters from the template file with the RavenParameters.dat file
+        self.param_matches = []
+        for line in rvp_template_clean:
+            for param in rvn_rvp_params[1:][0:]:
+                if param[0] in line and param[0].strip() != '':
+                    self.param_matches.append(param)
                     chk_params = QCheckBox()
                     chk_params.setChecked(True)
                     lbl_params = QLabel()
-                    lbl_params.setText(param)
+                    lbl_params.setText(param[0])
                     current_row = table.rowCount()  # Get the number of rows the table has
                     table.insertRow(current_row)  # Inserts a new row below the last row
                     table.setCellWidget(current_row, 0, chk_params)
@@ -80,17 +91,75 @@ class Ostrich:
         table = dlg.table_ost_values
 
         self.clear_table(table)
-
-        for param in params:
+        for line in params:
             current_row = table.rowCount()  # Get the number of rows the table has
             table.insertRow(current_row)  # Inserts a new row below the last row
+
+            initial_value = line[4]
+            lower_bound = line[5]
+            upper_bound = line[6]
+
+            try:
+                int(initial_value)
+                int(lower_bound)
+                int(upper_bound)
+
+                param_type = 'Integer'
+                initial_value = int(line[4])
+                lower_bound = int(line[5])
+                upper_bound = int(line[6])
+            except:
+                param_type = 'Real'
+                initial_value = float(line[4])
+                lower_bound = float(line[5])
+                upper_bound = float(line[6])
+
             lbl_param = QLabel()
-            lbl_param.setText(param)
+            lbl_param.setText(line[0])
             combo_param_type = QComboBox()
             combo_param_type.addItems(['Integer', 'Real'])
+            combo_param_type.setCurrentText(param_type)
+
+            txt_input = QLineEdit()
+            txt_output = QLineEdit()
+            txt_internal = QLineEdit()
+            txt_format = QLineEdit()
+
+            if param_type == 'Integer':
+                spin_initial_val = QSpinBox()
+                spin_lwr_bound = QSpinBox()
+                spin_upr_bound = QSpinBox()
+                txt_input.setEnabled(False)
+                txt_output.setEnabled(False)
+                txt_internal.setEnabled(False)
+                txt_format.setEnabled(False)
+            else:
+                spin_initial_val = QDoubleSpinBox()
+                spin_lwr_bound = QDoubleSpinBox()
+                spin_upr_bound = QDoubleSpinBox()
+
+            spin_initial_val.setMinimum(lower_bound)
+            spin_initial_val.setMaximum(upper_bound)
+            spin_initial_val.setValue(initial_value)
+
+            spin_lwr_bound.setMinimum(lower_bound)
+            spin_lwr_bound.setMaximum(upper_bound)
+            spin_lwr_bound.setValue(lower_bound)
+
+            spin_upr_bound.setMinimum(lower_bound)
+            spin_upr_bound.setMaximum(upper_bound)
+            spin_upr_bound.setValue(upper_bound)
 
             table.setCellWidget(current_row, 0, lbl_param)
             table.setCellWidget(current_row, 1, combo_param_type)
+            table.setCellWidget(current_row, 2, spin_initial_val)
+            table.setCellWidget(current_row, 3, spin_lwr_bound)
+            table.setCellWidget(current_row, 4, spin_upr_bound)
+            table.setCellWidget(current_row, 5, txt_input)
+            table.setCellWidget(current_row, 6, txt_output)
+            table.setCellWidget(current_row, 7, txt_internal)
+            table.setCellWidget(current_row, 8, txt_format)
+
         table.resizeColumnsToContents()
 
     def get_params(self, dlg):
@@ -99,9 +168,8 @@ class Ostrich:
         params = []
         for row in range(rows):
             checkbox = table.cellWidget(row, 0)
-            param = table.cellWidget(row, 1)
             if checkbox.isChecked():
-                params.append(param.text())
+                params.append(self.param_matches[row])
             else:
                 pass
 
